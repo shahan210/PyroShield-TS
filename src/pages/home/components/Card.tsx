cccf81a2264a76bbc0021fea8030de6f44924952
shahan210 <mohammedshahan210@gmail.com>
2024-09-24 13:03:09 +0400

    update: home services

--- src/pages/home/components/Card.tsx
+++ src/pages/home/components/Card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { cardService } from "./model";
 type Props = {
   image: string;
   desc: string;
@@ -9,10 +8,14 @@ const Card: React.FC<Props> = ({ image, desc }) => {
   return (
     <div className="services__card">
       <motion.img
-      initial={{ scale: 1 }}
-      whileHover={{ scale: 1.1 }}
-      transition={{ duration: 2 }}
-       src={image} alt={desc} className="services__img" />
+        initial={{ scale: 1 }}
+        whileHover={{ scale: 1.1 }}
+        loading="lazy"
+        transition={{ duration: 2 }}
+        src={image}
+        alt={desc}
+        className="services__img"
+      />
       <div className="services__title">{desc}</div>
     </div>
   );
